bdc741898eb63b3584bd36e63ca794d374e59dda
PMT201 <PMT201@PMT3011>
2026-03-13 11:09:17 +0700

    buoi2

---
+++ KyThuatLapTrinh/Buoi1/buoi1_1.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct Person {
+	int id;
+	string name;
+	int age;
+	string address;
+	
+};
+void ViewPresonList(vector<Person> p) {
+	if (p.size() == 0)
+		cout << "An empty list" << endl;
+	else {
+		for (int i = 0; i < p.size(); i++) {
+			cout << "Person " << (i + 1) << endl;
+			cout << "\t+ id:" << p[i].id << endl;
+			cout << "\t+ Name: " << p[i].name << endl;
+			cout << "\t+ Age:" << p[i].age << endl;
+			cout << "\t+ Address: " << p[i].address << endl;
+		}
+	}
+}
+void AddPerson(vector<Person>& p) {
+	Person a;
+	cout << "+ Id: ";
+	cin >> a.id;
+	cout << "+ Name: ";
+	cin.ignore();
+	getline(cin, a.name);
+	cout << "+ Age: ";
+	cin >> a.age;
+	cout << "+ Address: ";
+	cin.ignore();
+	getline(cin, a.address);
+	p.push_back(a);
+	cout << "Add a person successfully" << endl;
+
+}
+void RemovePerson(vector<Person>& p, int id) {
+	for (auto i = p.begin(); i != p.end(); i++) {
+		if (i->id == id) {
+			p.erase(i);
+			cout << "Remove a person successfuly" << endl;
+			return;
+		}
+	}
+}
+int main() {
+	vector<Person> list;
+	do {
+		system("cls");
+		cout << "---HUMAN RESOURCE--------" << endl;
+		cout << "1.View preson list" << endl;
+		cout << "2.Add a preson " << endl;
+		cout << "3. Remove a preson" << endl;
+		cout << "4.Find a preson by name" << endl;
+		cout << "5.Export to file" << endl;
+		cout << "6.Import from file" << endl;
+		cout << "0.Exit" << endl;
+		cout << "----------------------------" << endl;
+		cout << "Your command: ";
+		int cmd;
+		cin >> cmd;
+		switch (cmd)
+		{
+		case 1: {
+			ViewPresonList(list);
+			break;
+		}
+		case 2: {
+			AddPerson(list);
+			break;
+		}
+		case 3: {
+			int id;
+			cout << "Input ID to remove: ";
+			cin >> id;
+			RemovePerson(list, id);
+			break;
+		}
+		case 4: {
+			break;
+		}
+		case 5: {
+			break;
+		}
+		case 6: {
+			break;
+		}
+		case 0: {
+			return 0;
+		}
+		default:
+			cout << "Your command isn't found. Try again..." << endl;
+			break;
+		}
+		cout << "Press Enter to continue...";
+		cin.ignore();
+		cin.get();
+	} while (true);
+	return 0;
+}
